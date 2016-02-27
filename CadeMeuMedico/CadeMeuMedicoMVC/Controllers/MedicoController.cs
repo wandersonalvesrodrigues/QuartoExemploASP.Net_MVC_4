36dab672700913b30da15687f816cd83649d9188
Wanderson Alves Rodrigues <wandersonallves@yahoo.com.br>
2016-02-26 22:18:09 -0300

    Ajuste comentários

--- CadeMeuMedico/CadeMeuMedicoMVC/Controllers/MedicoController.cs
+++ CadeMeuMedico/CadeMeuMedicoMVC/Controllers/MedicoController.cs
@@ -23,6 +23,13 @@ namespace CadeMeuMedicoMVC.Controllers
             //No formulário de cadastro do Médicos,essas informações serão apresentadas em um ComboBox (ou Dropdownlist).No caso de HTML, 
             //o ComboBox é representado pelo elemento select.
             //Nas propriedades dinâmicas da ViewBag retornarmos já o elemento que será apresentado na View. Para isso utilizamos o helper SelectList
+            //Esta ViewBag recebe uma estrutura DropDownList que é criada através da classe SelectList e os parâmetros utilizados são: 
+            //Source, Value, Name (Origem do dado [Método ListarClientes], valor do elemento e nome a ser exibido). 
+            //Estes parâmetros precisam coincidir com as propriedades da estrutura do seu dado (classe Cliente).
+            //Para o Razor HtmlHelper criar corretamente seu DropDownList basta informar (no formato String) o 
+            //nome da ViewBag que contém a estrutura de dados, foi adicionado um segundo parâmetro para criar um elemento em 
+            //branco na primeira posição, assim evitando que o primeiro item seja selecionado por engano.
+
             ViewBag.IDCidade = new SelectList(allCidades, "IDCidade", "Nome");
             ViewBag.IDEspecialidade = new SelectList(allEspecialidades, "IDEspecialidade", "Nome");
 
